9f76ac8fd05b6bc6466e11ffe88ef605e13695f7
xinmiaoya <1172774037@qq.com>
2019-10-21 09:51:55 +0800

    10/21 9：51 by刘

---
+++ src/main/java/cn/edu/whut/msims/bean/SysmLawInfo.java
@@ -0,0 +1,123 @@
+package cn.edu.whut.msims.bean;
+
+import java.util.Date;
+
+public class SysmLawInfo {
+    private  String law_num;//发文字号
+    private  String law_class;//法律法规类别
+    private  String law_name;//法律法规标准名称
+    private  Date law_sdate;//颁布时间
+    private  Date law_edate;//实施时间
+    private  String law_dep;//颁布部门
+    private  String law_checker;//审核人
+    private  Date law_checktime;//审核时间
+    private  String law_note;//备注
+    private  String law_admin_id;//上传人
+    private  Date law_date;//上传时间
+
+
+    public String getLaw_num() {
+        return law_num;
+    }
+
+    public void setLaw_num(String law_num) {
+        this.law_num = law_num;
+    }
+
+    public String getLaw_class() {
+        return law_class;
+    }
+
+    public void setLaw_class(String law_class) {
+        this.law_class = law_class;
+    }
+
+    public String getLaw_name() {
+        return law_name;
+    }
+
+    public void setLaw_name(String law_name) {
+        this.law_name = law_name;
+    }
+
+    public Date getLaw_sdate() {
+        return law_sdate;
+    }
+
+    public void setLaw_sdate(Date law_sdate) {
+        this.law_sdate = law_sdate;
+    }
+
+    public Date getLaw_edate() {
+        return law_edate;
+    }
+
+    public void setLaw_edate(Date law_edate) {
+        this.law_edate = law_edate;
+    }
+
+    public String getLaw_dep() {
+        return law_dep;
+    }
+
+    public void setLaw_dep(String law_dep) {
+        this.law_dep = law_dep;
+    }
+
+    public String getLaw_checker() {
+        return law_checker;
+    }
+
+    public void setLaw_checker(String law_checker) {
+        this.law_checker = law_checker;
+    }
+
+    public Date getLaw_checktime() {
+        return law_checktime;
+    }
+
+    public void setLaw_checktime(Date law_checktime) {
+        this.law_checktime = law_checktime;
+    }
+
+    public String getLaw_note() {
+        return law_note;
+    }
+
+    public void setLaw_note(String law_note) {
+        this.law_note = law_note;
+    }
+
+    public String getLaw_admin_id() {
+        return law_admin_id;
+    }
+
+    public void setLaw_admin_id(String law_admin_id) {
+        this.law_admin_id = law_admin_id;
+    }
+
+    public Date getLaw_date() {
+        return law_date;
+    }
+
+    public void setLaw_date(Date law_date) {
+        this.law_date = law_date;
+    }
+
+    @Override
+    public String toString() {
+        return "SysmLawInfo{" +
+                "law_num='" + law_num + '\'' +
+                ", law_class='" + law_class + '\'' +
+                ", law_name='" + law_name + '\'' +
+                ", law_sdate=" + law_sdate +
+                ", law_edate=" + law_edate +
+                ", law_dep='" + law_dep + '\'' +
+                ", law_checker='" + law_checker + '\'' +
+                ", law_checktime=" + law_checktime +
+                ", law_note='" + law_note + '\'' +
+                ", law_admin_id='" + law_admin_id + '\'' +
+                ", law_date=" + law_date +
+                '}';
+    }
+}
---
+++ src/main/java/cn/edu/whut/msims/bean/SysmPreveInfo.java
@@ -0,0 +1,92 @@
+package cn.edu.whut.msims.bean;
+
+import java.util.Date;
+
+public class SysmPreveInfo {
+    private String preve_num;//发文字号
+    private String preve_name;//文件名称
+    private Date preve_sdate;//颁布时间
+    private Date preve_edate;//实施时间
+    private String preve_dep;//颁布部门
+    private String preve_path;//上传文件
+    private String admin_id;//上传人
+    private Date preve_date;//上传时间
+
+    public String getPreve_num() {
+        return preve_num;
+    }
+
+    public void setPreve_num(String preve_num) {
+        this.preve_num = preve_num;
+    }
+
+    public String getPreve_name() {
+        return preve_name;
+    }
+
+    public void setPreve_name(String preve_name) {
+        this.preve_name = preve_name;
+    }
+
+    public Date getPreve_sdate() {
+        return preve_sdate;
+    }
+
+    public void setPreve_sdate(Date preve_sdate) {
+        this.preve_sdate = preve_sdate;
+    }
+
+    public Date getPreve_edate() {
+        return preve_edate;
+    }
+
+    public void setPreve_edate(Date preve_edate) {
+        this.preve_edate = preve_edate;
+    }
+
+    public String getPreve_dep() {
+        return preve_dep;
+    }
+
+    public void setPreve_dep(String preve_dep) {
+        this.preve_dep = preve_dep;
+    }
+
+    public String getPreve_path() {
+        return preve_path;
+    }
+
+    public void setPreve_path(String preve_path) {
+        this.preve_path = preve_path;
+    }
+
+    public String getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(String admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public Date getPreve_date() {
+        return preve_date;
+    }
+
+    public void setPreve_date(Date preve_date) {
+        this.preve_date = preve_date;
+    }
+
+    @Override
+    public String toString() {
+        return "SysmPreveInfo{" +
+                "preve_num='" + preve_num + '\'' +
+                ", preve_name='" + preve_name + '\'' +
+                ", preve_sdate=" + preve_sdate +
+                ", preve_edate=" + preve_edate +
+                ", preve_dep='" + preve_dep + '\'' +
+                ", preve_path='" + preve_path + '\'' +
+                ", admin_id='" + admin_id + '\'' +
+                ", preve_date=" + preve_date +
+                '}';
+    }
+}
