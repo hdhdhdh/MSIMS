12248b972a8a69f3361f92cad58a884e6fc34b88
SFH <sunfahu@outlook.com>
2019-10-22 20:54:35 +0800

    sfh add jsp sample

--- src/main/java/cn/edu/whut/msims/controller/DnmDnController.java
+++ src/main/java/cn/edu/whut/msims/controller/DnmDnController.java
@@ -2,6 +2,7 @@ package cn.edu.whut.msims.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/dnmdn")
 public class DnmDnController {
