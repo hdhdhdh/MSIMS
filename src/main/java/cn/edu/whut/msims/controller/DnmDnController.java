b631be9d6f19f2216bdc57608acddf6595899910
XX <1049744401@qq.com>
2019-10-22 17:17:25 +0800

    xx页面设计

---
+++ src/main/java/cn/edu/whut/msims/controller/DnmDnController.java
@@ -0,0 +1,10 @@
+package cn.edu.whut.msims.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dnmdn")
+public class DnmDnController {
+
+}
