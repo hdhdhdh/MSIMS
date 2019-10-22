0bd049f9a8d43b5f67757f3206b5aa40b35f4cc0
xinmiaoya <1172774037@qq.com>
2019-10-22 16:18:06 +0800

    10/22 16:17 by刘

--- src/main/java/cn/edu/whut/msims/dao/SysmLawDao.java
+++ src/main/java/cn/edu/whut/msims/dao/SysmLawDao.java
@@ -1,8 +1,7 @@
 package cn.edu.whut.msims.dao;
 
 import cn.edu.whut.msims.bean.SysmLawInfo;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,9 +9,19 @@ import java.util.List;
 @Component
 @Mapper
 public interface SysmLawDao {
+
+    @Select("SELECT * FROM tb_law")
     public List<SysmLawInfo> findAllLaw();//查询所有安全生产法律法规
+
+    @Select("SELECT * FROM tb_law where law_num=#{law_num}")
     public SysmLawInfo findLawById(String law_num);//通过发文字号查询安全生产法律法规
+
+    @Insert("INSERT INTO tb_law(law_num,law_class,law_name,law_sdate,law_edate,law_dep,law_checker,law_checktime,law_note,admin_id,law_date) values(#{law_num},#{law_class},#{law_name},#{law_sdate}，#{law_edate}，#{law_dep}，#{law_checker}，#{law_checktime}，#{law_note}，#{admin_id}，#{law_date})")
     public int addLaw(SysmLawInfo sysmLawInfo);//增加安全生产法律法规
+
+    @Update("UPDATE tb_law SET law_class=#{law_class},law_name=#{law_name},law_sdate=#{law_sdate},law_edate=#{law_edate},law_dep=#{law_dep},law_checker=#{law_checker},law_checktime=#{law_checktime},law_note=#{law_note},admin_id=#{admin_id},law_date=#{law_date} WHERE law_num=#{law_num}")
     public void delLaw(String law_num);//删除安全生产法律法规
+
+    @Delete("DELETE FROM tb_law WHERE law_num=#{law_num}")
     public int updateLaw(SysmLawInfo sysmLawInfo);//更新安全生产法律法规
 }
--- src/main/java/cn/edu/whut/msims/dao/SysmPreveDao.java
+++ src/main/java/cn/edu/whut/msims/dao/SysmPreveDao.java
@@ -1,7 +1,7 @@
 package cn.edu.whut.msims.dao;
 
 import cn.edu.whut.msims.bean.SysmPreveInfo;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,9 +9,19 @@ import java.util.List;
 @Component
 @Mapper
 public interface SysmPreveDao {
+
+    @Select("SELECT * FROM tb_preve")
     public List<SysmPreveInfo> findAllPreve();//查询所有双重预防机制文件
+
+    @Select("SELECT * FROM tb_preve where preve_num=#{preve_num}")
     public SysmPreveInfo findPreveById(String preve_num);//通过发文字号查询双重预防机制文件
+
+    @Insert("INSERT INTO tb_preve(preve_num,preve_name,preve_sdate,preve_edate,preve_dep,preve_path,admin_id,preve_date) values(#{preve_num},#{preve_name},#{preve_sdate},#{preve_edate}，#{preve_dep}，#{preve_path}，#{admin_id}，#{preve_date})")
     public int addPreve(SysmPreveInfo sysmPreveInfo);//增加双重预防机制文件
+
+    @Update("UPDATE tb_preve SET preve_name=#{preve_name},preve_sdate=#{preve_sdate},preve_edate=#{preve_edate},preve_dep=#{preve_dep},preve_path=#{preve_path},admin_id=#{admin_id},preve_date=#{preve_date} WHERE preve_num=#{preve_num}")
     public void delPreve(String preve_num);//删除双重预防机制文件
+
+    @Delete("DELETE FROM tb_preve WHERE preve_num=#{preve_num}")
     public int updatePreve(SysmPreveInfo sysmPreveInfo);//更新双重预防机制文件
 }
